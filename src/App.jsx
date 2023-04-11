a1fcc2ce4a7333fc49f327f1f1abf477a44d2105
drashti1112 <425928-drashti1112@users.noreply.replit.com>
2023-04-11 12:29:46 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import Header from './Header'
+import Footer from './Footer'
+import Employees from './Employees'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Employees />
+      <Footer />
+    </>
+  )
+}
